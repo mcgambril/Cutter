5898a88dd3559df60ff1917a6168f4bcdee6e34b
mcgambril <mcgambril@crimson.ua.edu>
2015-07-28 21:08:46 -0500

    jQuery
    Now calling the proper, up to date, jQuery file

--- application/views/header_view.php
+++ application/views/header_view.php
@@ -7,15 +7,17 @@
  */
  -->
 
+<html>
+
 <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
 
-<html>
 <head>
 <title>Cutter Home</title>
 </head>
 
-<script type="text/javascript" src="<?php echo base_url("assets/js/jQuery-1.10.2.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url("assets/js/jQuery-1.11.3.js"); ?>"></script>
 <script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
+
 <style type="text/css">
     body {
         padding-top: 60px;
