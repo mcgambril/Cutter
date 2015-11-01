c004061c10bb15193881b5e2163f84d64138090d
mcgambril <mcgambril@crimson.ua.edu>
2015-10-31 23:57:47 -0500

    New score posting works
    did not have to change original score submit post function to work with
    new view.  Seems if it tries to post an element that doesn't exist, it
    just runs through the loop and ignores...so no problem i suppose...we
    shall see

--- application/views/score_post2_view.php
+++ application/views/score_post2_view.php
@@ -22,7 +22,8 @@
         <div class="row">
 
             <div class="col-md-3">
-                <?php echo '<p>Date: <input type="text" value="' . $date . '" name="datepicker" id="datepicker"  class="form-control" data-date-format="yyyy-mm-dd"></p>';?>
+
+                <?php echo '<p>Date: <input type="text" value="' . $date . '" name="datepicker" id="datepicker"  class="form-control" data-date-format="yyyy-mm-dd" readonly></p>';?>
                 <br />
                 <label for="pick-course">Course:</label>
                 <select class="form-control" id="pick-course" name="course">
@@ -61,18 +62,66 @@
                                 echo '<thead>';
                                 echo '</thead>';
                                 echo '<tbody>';
-                                    echo '<tr>';
+                                    if ($row->scoreSummary == 'empty'){
+                                        echo '<tr>';
                                         echo '<td class="col-md-2">' . $row->playerName . '<input type="hidden" name="' . $row->playerID . '" value="' . $row->playerID . '" /></td>';
                                         echo '<td class="col-md-2">';
-                                            echo '<input type="checkbox" id="' . $row->playerID . '-played" data-toggle="collapse" data-target=".' . $row->playerID . 'score"/>Yes';
+                                        echo '<input type="checkbox" id="' . $row->playerID . '-played" data-toggle="collapse" data-target=".' . $row->playerID . 'score"/>Yes';
                                         echo '</td>';
                                         echo '<td class="col-md-2">Empty';
-                                            echo '<input type="text" name="' . $row->playerID . 'am-score"  id="' . $row->playerID . 'am-score" class="form-control collapse ' . $row->playerID . 'score">';
+                                        echo '<input type="text" name="' . $row->playerID . 'am-score"  id="' . $row->playerID . 'am-score" class="form-control collapse ' . $row->playerID . 'score">';
                                         echo '</td>';
                                         echo '<td class="col-md-2">Empty';
-                                            echo '<input type="text" name="' . $row->playerID . 'pm-score" id ="' . $row->playerID . 'pm-score" class="form-control collapse ' . $row->playerID . 'score">';
+                                        echo '<input type="text" name="' . $row->playerID . 'pm-score" id ="' . $row->playerID . 'pm-score" class="form-control collapse ' . $row->playerID . 'score">';
                                         echo '</td>';
-                                    echo '</tr>';
+                                        echo '</tr>';
+                                    }
+                                    else if ($row->scoreSummary == 'am empty'){
+                                        echo '<tr>';
+                                        echo '<td class="col-md-2">' . $row->playerName . '<input type="hidden" name="' . $row->playerID . '" value="' . $row->playerID . '" /></td>';
+                                        echo '<td class="col-md-2">';
+                                        echo '<input type="checkbox" id="' . $row->playerID . '-played" data-toggle="collapse" data-target=".' . $row->playerID . 'score"/>Yes';
+                                        echo '</td>';
+                                        echo '<td class="col-md-2">Empty';
+                                        echo '<input type="text" name="' . $row->playerID . 'am-score"  id="' . $row->playerID . 'am-score" class="form-control collapse ' . $row->playerID . 'score">';
+                                        echo '</td>';
+                                        foreach ($row->pmScore as $score){
+                                            echo '<td class="col-md-2">' . $score->scoreScore . '';
+                                            echo '</td>';
+                                        }
+                                        echo '</tr>';
+                                    }
+                                    else if ($row->scoreSummary == 'pm empty'){
+                                        echo '<tr>';
+                                        echo '<td class="col-md-2">' . $row->playerName . '<input type="hidden" name="' . $row->playerID . '" value="' . $row->playerID . '" /></td>';
+                                        echo '<td class="col-md-2">';
+                                        echo '<input type="checkbox" id="' . $row->playerID . '-played" data-toggle="collapse" data-target=".' . $row->playerID . 'score"/>Yes';
+                                        echo '</td>';
+                                        foreach($row->amScore as $score) {
+                                            echo '<td class="col-md-2">' . $score->scoreScore . '';
+                                            echo '</td>';
+                                        }
+                                        echo '<td class="col-md-2">Empty';
+                                        echo '<input type="text" name="' . $row->playerID . 'pm-score" id ="' . $row->playerID . 'pm-score" class="form-control collapse ' . $row->playerID . 'score">';
+                                        echo '</td>';
+                                    }
+                                    else if ($row->scoreSummary == 'full'){
+                                        echo '<tr>';
+                                        echo '<td class="col-md-2">' . $row->playerName . '<input type="hidden" name="' . $row->playerID . '" value="' . $row->playerID . '" /></td>';
+                                        echo '<td class="col-md-2">';
+                                        echo '<input type="checkbox" id="' . $row->playerID . '-played" data-toggle="collapse" data-target=".' . $row->playerID . '" disabled="disabled"/>No';
+                                        echo '</td>';
+                                        foreach($row->amScore as $score) {
+                                            echo '<td class="col-md-2">' . $score->scoreScore . '';
+                                            echo '</td>';
+                                        }
+                                        foreach ($row->pmScore as $score){
+                                            echo '<td class="col-md-2">' . $score->scoreScore . '';
+                                            echo '</td>';
+                                        }
+                                        echo '</tr>';
+                                    }
+
                                 echo '</tbody>';
                             echo '</table>';
                         echo '</div>';
