249a3daa86c6a359465bc3a6289a4c1c20bdd5ea
mcgambril <mcgambril@crimson.ua.edu>
2016-01-08 02:14:10 -0600

    handicap update jquery dialog and routing working

--- application/views/handicap_update_view.php
+++ application/views/handicap_update_view.php
@@ -17,8 +17,17 @@
                 <h3>Do you want to Update all Players' Handicaps?</h3>
                 <br>
                 <p class="col-md-4"></p>
-                <a class="btn btn-default col-md-2" href="<?php echo base_url("index.php/handicap/submitUpdate"); ?>">Yes. Update Handicaps</a>
-                <a class="btn btn-default col-md-2" href="<?php echo base_url("index.php/handicap/index"); ?>">Back</a>
+                <!--<a class="btn btn-default col-md-2" onclick="return confirmSubmit(this.form)" href="<?php /*echo base_url("index.php/handicap/submitUpdate"); */?>">Yes. Update Handicaps</a>-->
+                <?php
+                    /*$onclick = array('onclick' => "return confirm('Are you sure?')");
+                    $submitUpdateLink = base_url("index.php/handicap/submitUpdate");*/
+                    //anchor($submitUpdateLink, 'Yes. Update Handicaps', $onclick);
+                ?>
+                <!--<a class="btn btn-default col-md-2" href="<?php /*echo base_url("index.php/handicap/submitUpdate"); */?>" onclick="return confirmHandicapUpdate();">Yes. Update Handicaps</a>-->
+                <a class="btn btn-default col-md-2" id="openHandicapDialog">Yes. Update Handicaps</a>
+                <div id="dialog-confirm" title="Are you sure?">Click continue to update every player's handicap.</div>
+                <!--<a class="btn btn-default col-md-2" ><?php /*echo anchor($submitUpdateLink, 'Yes. Update Handicaps', $onclick); */?></a>-->
+                <a class="btn btn-default col-md-2" href="<?php echo base_url("index.php/"); ?>">Home</a>
                 <p class="col-md-4"></p>
                 <br><br><br>
             </div>
--- assets/js/site.js
+++ assets/js/site.js
@@ -25,6 +25,40 @@ $(function() {
     });
 });
 
+//able to derive solution for the confirm pop up box through onclick using the following 2 links
+//http://uly.me/codeigniter-anchor-with-onclick/
+//http://stackoverflow.com/questions/24756248/codeigniter-before-submit-confirm-popup-box-using-javascript
+function confirmHandicapUpdate() {
+    return confirm("Are you sure you want to Update all Player Handicaps?");
+}
+
+$(function dialogHandicapUpdate(){
+    var $currentPath = window.location.pathname;        //  /handicap/update
+    var $length = $currentPath.length;
+    var $endPos = $length - 6;    //take 6 away b/c 'update' is 6 letters;
+    var $tempPath = $currentPath.substring(0,$endPos);    //     /handicap/  b/c endPos is where the string cuts off...will not take that letter
+    var $newPath = $tempPath.concat("submitUpdate");
+    $( "#dialog-confirm").dialog({
+        height: 200,
+        draggable: true,
+        modal: true,
+        resizable: true,
+        autoOpen: false,
+        buttons: {
+            "Continue": function() {
+                /*$( this).dialog( "close" );*/
+                window.location.href=$newPath;
+            },
+            Cancel: function() {
+                $( this).dialog( "close" );
+            }
+        }
+    });
+    $( "#openHandicapDialog").click(function() {
+        $( "#dialog-confirm").dialog( "open" );
+    })
+});
+
 
 
 
