8d7a476f9f22af3af1bb0e93c4e07aa74a2f4d30
Matthew Gambril <matthew.gambril@gmail.com>
2018-07-22 23:18:55 -0500

    Fixed Copyright to screen bottom

--- application/views/footer_view.php
+++ application/views/footer_view.php
@@ -6,11 +6,11 @@
  * Time: 9:33 PM
  */
  -->
-<div class="text-center">
+<div class="text-center" style="position: relative">
     <?php
         date_default_timezone_set('America/Mexico_City');
     ?>
-    <p>Copyright 2015 - <?php echo date("Y"); ?></p>
+    <p style="position: fixed; bottom: 0; text-align: center; width: 100%">Copyright 2015 - <?php echo date("Y"); ?></p>
 </div>
 
 </body>
