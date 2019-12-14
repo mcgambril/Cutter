a298005616e403953ee87e1c821d31490c298c59
Matthew Gambril <matthew.gambril@gmail.com>
2019-12-14 16:14:18 -0600

    Reconnecting Github
    Fixed footer positioning.  Also trying to see if my github connection works.

--- application/views/footer_view.php
+++ application/views/footer_view.php
@@ -10,7 +10,8 @@
     <?php
         date_default_timezone_set('America/Mexico_City');
     ?>
-    <p style="position: fixed; bottom: 0; text-align: center; width: 100%">Copyright 2015 - <?php echo date("Y"); ?></p>
+    <!--<p style="position: fixed; bottom: 0; text-align: center; width: 100%">Copyright 2015 - <?php echo date("Y"); ?></p>--->
+    <p>Copyright 2015 - <?php echo date("Y"); ?></p>
 </div>
 
 </body>
